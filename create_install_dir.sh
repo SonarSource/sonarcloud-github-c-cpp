2c8be3a4a34728b16f12554cf61d10e67c93f92e
Tomasz Kamiński <tomasz.kamiski@sonarsource.com>
2022-11-03 09:39:33 +0100

    Test if installation dir is directory

--- create_install_dir.sh
+++ create_install_dir.sh
@@ -7,12 +7,20 @@ echo "Installation dir is '${INSTALL_DIR}'"
 test ! -z "${INSTALL_DIR}"
 check_status "Empty installation-dir specified"
 
-mkdir -p "${INSTALL_DIR}"
-check_status "Failed to create non-existing installation-dir '${INSTALL_DIR}'"
+if [[ ! -e "${INSTALL_DIR}" ]]; then
+  mkdir -p "${INSTALL_DIR}"
+  check_status "Failed to create non-existing installation-dir '${INSTALL_DIR}'"
+fi  
+
+ABSOLUTE_INSTALL_PATH=$(realpath "${INSTALL_DIR}")
+echo "Absolute installation path is '${ABSOLUTE_INSTALL_PATH}'"
+
+test  -d ${INSTALL_DIR}
+check_status "Installation-dir '${INSTALL_DIR}' is not a directory (absolute path is '${ABSOLUTE_INSTALL_PATH}')"
 
 test -r "${INSTALL_DIR}"
-check_status "Installation-dir '${INSTALL_DIR}' is not readable (absolute path is '$(realpath "${INSTALL_DIR}")')"
+check_status "Installation-dir '${INSTALL_DIR}' is not readable (absolute path is '${ABSOLUTE_INSTALL_PATH}')"
 
 test -w "${INSTALL_DIR}"
-check_status "Installation-dir '${INSTALL_DIR}' is not writeable (absolute path is '$(realpath "${INSTALL_DIR}")')"
+check_status "Installation-dir '${INSTALL_DIR}' is not writeable (absolute path is '${ABSOLUTE_INSTALL_PATH}')"
 
