79375876044bab772d95d167e607d3221d74c38a
Tomasz Kamiński <tomasz.kamiski@sonarsource.com>
2022-11-03 08:40:07 +0100

    Include abosolute path in error for unacessible dirs

--- create_install_dir.sh
+++ create_install_dir.sh
@@ -11,8 +11,8 @@ mkdir -p "${INSTALL_DIR}"
 check_status "Failed to create non-existing installation-dir '${INSTALL_DIR}'"
 
 test -r "${INSTALL_DIR}"
-check_status "Installation-dir '${INSTALL_DIR}' is not readable"
+check_status "Installation-dir '${INSTALL_DIR}' is not readable (absolute path is '$(realpath "${INSTALL_DIR}")')"
 
 test -w "${INSTALL_DIR}"
-check_status "Installation-dir '${INSTALL_DIR}' is not writeable"
+check_status "Installation-dir '${INSTALL_DIR}' is not writeable (absolute path is '$(realpath "${INSTALL_DIR}")')"
 
