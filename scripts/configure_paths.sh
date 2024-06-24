e25edaefff1e076daf6517d462ed86f8c99fec70
Vincenzo Pellegrini <vincenzo.pellegrini@sonarsource.com>
2024-06-24 15:42:07 +0200

    SCCPPGHA-6 Support Running on Linux ARM64 (#57)
    Co-authored-by: Michael Jabbour <117195239+michael-jabbour-sonarsource@users.noreply.github.com>

--- scripts/configure_paths.sh
+++ scripts/configure_paths.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ ${ARCH} != "X64" && ! ( ${OS} == "macOS" && ${ARCH} == "ARM64" ) ]]; then 
+if [[ ${ARCH} != "X64" && ! (${ARCH} == "ARM64" && (${OS} == "macOS" || ${OS} == "Linux")) ]]; then 
   echo "::error::Architecture '${ARCH}' is unsupported by build-wrapper"
   exit 1
 fi
 
 case ${OS} in
   Windows)
-    SONAR_SCANNER_SUFFIX="windows"
+    SONAR_SCANNER_SUFFIX="-windows"
     BUILD_WRAPPER_SUFFIX="win-x86"
     SONAR_SCANNER_NAME="sonar-scanner.bat"
     BUILD_WRAPPER_NAME="build-wrapper-win-x86-64.exe"
@@ -15,15 +15,26 @@ case ${OS} in
     SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_WINDOWS}"
     ;;
   Linux)  
-    SONAR_SCANNER_SUFFIX="linux"
-    BUILD_WRAPPER_SUFFIX="linux-x86"
+    case ${ARCH} in
+      X64)
+        SONAR_SCANNER_SUFFIX="-linux"
+        BUILD_WRAPPER_SUFFIX="linux-x86"
+        BUILD_WRAPPER_NAME="build-wrapper-linux-x86-64"
+        SONAR_SCANNER_URL="${SONAR_SCANNER_URL_LINUX}"
+        SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_LINUX}"
+        ;;
+      ARM64)
+        SONAR_SCANNER_SUFFIX=""
+        BUILD_WRAPPER_SUFFIX="linux-aarch64"
+        BUILD_WRAPPER_NAME="build-wrapper-linux-aarch64"
+        SONAR_SCANNER_URL="${SONAR_SCANNER_URL_UNIVERSAL}"
+        SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_UNIVERSAL}"
+        ;;
+    esac
     SONAR_SCANNER_NAME="sonar-scanner"
-    BUILD_WRAPPER_NAME="build-wrapper-linux-x86-64"
-    SONAR_SCANNER_URL="${SONAR_SCANNER_URL_LINUX}"
-    SONAR_SCANNER_SHA="${SONAR_SCANNER_SHA_LINUX}"
     ;;
   macOS)
-    SONAR_SCANNER_SUFFIX="macosx"
+    SONAR_SCANNER_SUFFIX="-macosx"
     BUILD_WRAPPER_SUFFIX="macosx-x86"
     SONAR_SCANNER_NAME="sonar-scanner"
     BUILD_WRAPPER_NAME="build-wrapper-macosx-x86"
@@ -40,7 +51,7 @@ esac
 echo "sonar-scanner-url=${SONAR_SCANNER_URL}"
 echo "sonar-scanner-sha=${SONAR_SCANNER_SHA}"
 
-SONAR_SCANNER_DIR="${INSTALL_PATH}/sonar-scanner-${SONAR_SCANNER_VERSION}-${SONAR_SCANNER_SUFFIX}"
+SONAR_SCANNER_DIR="${INSTALL_PATH}/sonar-scanner-${SONAR_SCANNER_VERSION}${SONAR_SCANNER_SUFFIX}"
 echo "sonar-scanner-dir=${SONAR_SCANNER_DIR}"
 echo "sonar-scanner-bin=${SONAR_SCANNER_DIR}/bin/${SONAR_SCANNER_NAME}"
 
